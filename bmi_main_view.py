899d987500999a01a04a623e0ea717970da14066
Mateusz Kacprzak <matkac98@gmail.com>
2021-04-24 11:23:35 +0200

    Poprawnie działająca wersja na PC

--- bmi_main_view.py
+++ bmi_main_view.py
@@ -18,49 +18,42 @@ def check_bmi(given_weight, given_height):
     result = given_weight / (given_height * given_height)
     if result < 15 :
         easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wygłodzenie')
+        second_try()
     if result > 15.1 and result <= 17.4:
         easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wychudzenie')
+        second_try()
     if result > 17.5 and result <=18.5:
         easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - niedowaga')
+        second_try()
     if result > 18.5 and result <= 24.9:
         easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wartość prawidłowa')
+        second_try()
     if result > 25 and result <= 29.9:
         easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - nadwaga')
+        second_try()
     if result > 30.0 and result <= 34.9:
         easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - I stopień otyłości')
+        second_try()
     if result > 35.0 and result <= 39.9:
         easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - II stopień otyłości')
+        second_try()
     if result > 40.0:
         easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - III stopień otyłości')
-
+        second_try()
+def second_try():
+    choice = easygui.buttonbox('Co chcesz zrobić ?', 'Your BMI status', ['Sprawdz BMI','BMI w Polsce','Zamknij program'])
+    if choice == 'Sprawdz BMI':
+        weight = int(easygui.enterbox('Podaj wagę: '))
+        height = cm_to_m(float(easygui.enterbox('Podaj wzrost w centymentrach: ')))
+        check_bmi(weight, height)
+    if choice == 'BMI w Polsce':
+        print(show_image())
+        second_try()
+    if choice == 'Zamknij program':
+        easygui.msgbox('3maj forme, life if life ','Exit window')
+        time.sleep(3)
 w=easygui.buttonbox('Chcesz sprawdzić swoje BMI', 'Your BMI status',['Tak','Zamknij program '])
 if w=='Tak':
     weight = int(easygui.enterbox('Podaj wagę: '))
     height = cm_to_m(float(easygui.enterbox('Podaj wzrost w centymentrach: ')))
-    result = weight/(height*height)
-    if result < 15 :
-        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wygłodzenie')
-        print(show_image())
-    if result > 15.1 and result <= 17.4:
-        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wychudzenie')
-        print(show_image())
-    if result > 17.5 and result <=18.5:
-        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - niedowaga')
-        print(show_image())
-    if result > 18.5 and result <= 24.9:
-        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wartość prawidłowa')
-        print(show_image())
-    if result > 25 and result <= 29.9:
-        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - nadwaga')
-        print(show_image())
-    if result > 30.0 and result <= 34.9:
-        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - I stopień otyłości')
-        print(show_image())
-    if result > 35.0 and result <= 39.9:
-        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - II stopień otyłości')
-        print(show_image())
-    if result > 40.0:
-        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - III stopień otyłości')
-        print(show_image())
-
-
+    check_bmi(weight, height)
