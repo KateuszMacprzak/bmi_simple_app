ce9cdd76eef33bac8db1fd53b891a3d8cbe7b26e
Mateusz Kacprzak <matkac98@gmail.com>
2021-04-24 10:52:36 +0200

    Dodanie funkcji check_bmi

--- bmi_main_view.py
+++ bmi_main_view.py
@@ -14,7 +14,26 @@ def show_image():
     plot.blit(obrazek, [0, 0])
     pygame.display.flip()
     time.sleep(3)
-w=easygui.buttonbox('Chcesz sprawdzić swoje BMI', 'Your BMI status',['Tak','Zamknij program'])
+def check_bmi(given_weight, given_height):
+    result = given_weight / (given_height * given_height)
+    if result < 15 :
+        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wygłodzenie')
+    if result > 15.1 and result <= 17.4:
+        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wychudzenie')
+    if result > 17.5 and result <=18.5:
+        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - niedowaga')
+    if result > 18.5 and result <= 24.9:
+        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - wartość prawidłowa')
+    if result > 25 and result <= 29.9:
+        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - nadwaga')
+    if result > 30.0 and result <= 34.9:
+        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - I stopień otyłości')
+    if result > 35.0 and result <= 39.9:
+        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - II stopień otyłości')
+    if result > 40.0:
+        easygui.msgbox(f'Twoje BMI wynosi {round(result,2)} - III stopień otyłości')
+
+w=easygui.buttonbox('Chcesz sprawdzić swoje BMI', 'Your BMI status',['Tak','Zamknij program '])
 if w=='Tak':
     weight = int(easygui.enterbox('Podaj wagę: '))
     height = cm_to_m(float(easygui.enterbox('Podaj wzrost w centymentrach: ')))
